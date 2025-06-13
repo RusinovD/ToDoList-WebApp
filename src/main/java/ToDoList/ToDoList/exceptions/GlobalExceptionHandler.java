e9a7b129b7040b466457490b67136a15110ed780
RusinovD <rusinovdu@bk.ru>
2025-06-13 11:23:25 +0500

    Добавил логирование в RestControllerAdvice

--- src/main/java/ToDoList/ToDoList/exceptions/GlobalExceptionHandler.java
+++ src/main/java/ToDoList/ToDoList/exceptions/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package ToDoList.ToDoList.exceptions;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -11,27 +12,31 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.time.format.DateTimeParseException;
-
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(IllegalStatusFormatException.class)
     public ResponseEntity<String> handleIllegalStatusFormatException(IllegalStatusFormatException e) {
+        log.error(e.getMessage(), e);
         return new ResponseEntity<> (e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(TaskNotFoundException.class)
     public ResponseEntity<String> handleTaskNotFoundException(TaskNotFoundException e) {
+        log.error(e.getMessage(), e);
         return new ResponseEntity<> (e.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(UserAlreadyExistException.class)
     public ResponseEntity<String> handleUserAlreadyExistException(UserAlreadyExistException e) {
+        log.error(e.getMessage(), e);
         return new ResponseEntity<> (e.getMessage(), HttpStatus.CONFLICT);
     }
 
     @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity<String> handleUserNotFoundException(UserNotFoundException e) {
+        log.error(e.getMessage(), e);
         return new ResponseEntity<> (e.getMessage(), HttpStatus.NOT_FOUND);
     }
 
@@ -42,26 +47,31 @@ public class GlobalExceptionHandler {
         if (e.getBindingResult().hasErrors()) {
             errorMessage = e.getBindingResult().getFieldErrors().get(0).getDefaultMessage();
         }
+        log.error(e.getMessage(), e);
         return new ResponseError (errorMessage);
     }
 
     @ExceptionHandler(DateTimeParseException.class)
     public ResponseEntity<String> handleDateTimeParseException(DateTimeParseException e) {
+        log.error(e.getMessage(), e);
         return new ResponseEntity<> ("Неверный формат даты, впишите дату в формате ГГГГ-ММ-ДД", HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        log.error(e.getMessage(), e);
         return new ResponseEntity<> (e.getMessage(), HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
     public ResponseEntity<String> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
+        log.error(e.getMessage(), e);
         return new ResponseEntity<> ("Не верный формат статуса, впишите \"TODO\", \"IN_PROGRESS\" или \"DONE\" ", HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(MissingServletRequestParameterException.class)
     public ResponseEntity<String> handleMissingServletRequestParameterException(MissingServletRequestParameterException e) {
+        log.error(e.getMessage(), e);
         return new ResponseEntity<> (e.getMessage(), HttpStatus.NOT_FOUND);
     }
 
